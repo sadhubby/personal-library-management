f04e7f545f4b45f13082915863d45662501171ee
sadhubby <iamthegreatgamerx.silent@gmail.com>
2025-04-16 00:48:29 +0800

    chore: init source code

---
+++ Main.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class Main{
+public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    
+    System.out.println("Enter input: ");
+    sc.nextLine();
+
+    sc.close();
+
+    }
+}
