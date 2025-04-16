e2e0c2b4d80e4fd85924050a5b5c2690f3416570
sadhubby <iamthegreatgamerx.silent@gmail.com>
2025-04-16 17:06:11 +0800

    test: testing package classes if working

--- Main.java
+++ Main.java
@@ -1,13 +1,24 @@
 import java.util.*;
-
+import library_package.*;
 class Main{
 public static void main(String[] args){
     Scanner sc = new Scanner(System.in);
-    
     System.out.println("Enter input: ");
-    sc.nextLine();
+    String bookName = sc.nextLine();
+    String bookAuthor = sc.nextLine();
+    int noOfPages = sc.nextInt();
 
     sc.close();
+    
+    Book book = new Book();
 
-    }
+    book.setBookName(bookName);
+    book.setAuthor(bookAuthor);
+    book.setNoOfPages(noOfPages);
+
+    System.out.println(book.getBookName() + " by: " + book.getAuthor() + "\nPage Counter: " + book.getNoOfPages());
+
+
+
+}
 }
