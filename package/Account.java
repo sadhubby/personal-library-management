87d5617b71424356fe90843303661d0b9ea5fd09
sadhubby <iamthegreatgamerx.silent@gmail.com>
2025-04-16 16:38:01 +0800

    chore(Account): added class attributes and methods

--- package/Account.java
+++ package/Account.java
@@ -1,6 +1,33 @@
 public class Account {
     // username, password
     // do i login or not here? maybe no because I just want to create blueprint for my objects. 
+    private String accountUsername;
+    private String accountEmail;
+    private String accountPassword;
 
-    
+    //Setters and getters
+
+    public void setAccountUsername (String acquiredAccountUsername){
+        this.accountUsername = acquiredAccountUsername;
+    }
+
+    public String getAccountUsername(){
+        return accountUsername;
+    }
+
+    public void setAccountEmail (String acquiredAccountEmail){
+        this.accountEmail = acquiredAccountEmail;
+    }
+
+    public String getAccountEmail(){
+        return accountEmail;
+    }
+
+    public void setAccountPassword (String acquiredPassword){
+        this.accountPassword = acquiredPassword;
+    }
+
+    public String getAccountPassword(){
+        return accountPassword;
+    }
 }
