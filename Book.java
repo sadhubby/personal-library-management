c3b770958dda1658a69bd4576d08766b543f67bf
sadhubby <iamthegreatgamerx.silent@gmail.com>
2025-04-16 15:52:08 +0800

    chore(Book): added class attributes and methods

--- Book.java
+++ Book.java
@@ -1,3 +1,43 @@
+// Class used to create class instance of a Book
 public class Book {
     
+    // Book Attributes
+    private String bookName;
+    private String author;
+    private String bookCoverUrl;
+    private int noOfPages;  
+    
+    // Setters and Getters
+    public void setBookName(String acquiredBookName){
+        this.bookName = acquiredBookName;
+    }
+
+    public String getBookName(){
+        return bookName;
+    }
+
+    public void setAuthor(String acquiredAuthor){
+        this.author = acquiredAuthor;
+    }
+
+    public String getAuthor(){
+        return author;
+    }
+
+    public void setBookCoverUrl(String acquiredBookCoverUrl){
+        this.bookCoverUrl = acquiredBookCoverUrl;
+    }
+
+    public String getBookCoverUrl(){
+        return bookCoverUrl;
+    }
+
+    public void setNoOfPages(int acquiredNoOfPages){
+        this.noOfPages = acquiredNoOfPages;
+    }
+
+    public int getNoOfPages(){
+        return noOfPages;
+    }
+
 }
