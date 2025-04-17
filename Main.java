0a235f3761aea6f961aa021a140d675b0e8c0a8e
sadhubby <iamthegreatgamerx.silent@gmail.com>
2025-04-17 22:55:10 +0800

    feat: adding books into personal library

--- Main.java
+++ Main.java
@@ -2,23 +2,39 @@ import java.util.*;
 import library_package.*;
 class Main{
 public static void main(String[] args){
-    Scanner sc = new Scanner(System.in);
-    System.out.println("Enter input: ");
-    String bookName = sc.nextLine();
-    String bookAuthor = sc.nextLine();
-    int noOfPages = sc.nextInt();
 
-    sc.close();
+        Scanner sc = new Scanner(System.in);
+        List<Book> library = new ArrayList<>();       
+        
+        for(int i = 0; i <= 5; i++){ //for loop for testing
+        System.out.println("Enter book name:");
+        String bookName = sc.nextLine();
+        
+        System.out.println("Enter author:");
+        String bookAuthor = sc.nextLine();
+        
+        System.out.println("Enter number of pages:");
+        int noOfPages = sc.nextInt();
     
-    Book book = new Book();
+        sc.nextLine();
+        
+        Book book = new Book();
+        book.setBookName(bookName);
+        book.setAuthor(bookAuthor);
+        book.setNoOfPages(noOfPages);
+        
 
-    book.setBookName(bookName);
-    book.setAuthor(bookAuthor);
-    book.setNoOfPages(noOfPages);
+        System.out.println("\nBook Details:");
+        System.out.println(book.getBookName() + " by: " + book.getAuthor() + "\nPage Counter: " + book.getNoOfPages());
+        
+        library.add(book);
+        
+        }
+        System.out.println("\nLibrary contains the following books:");
+        for (Book b : library) {
+            System.out.println(b.getBookName() + " by " + b.getAuthor());
 
-    System.out.println(book.getBookName() + " by: " + book.getAuthor() + "\nPage Counter: " + book.getNoOfPages());
-
-
-
-}
+        }
+        sc.close();
+    }
 }
