44fdd02f59badb2ed3b55aeeb7a7c66bf52e14b8
sadhubby <iamthegreatgamerx.silent@gmail.com>
2025-04-20 09:53:25 +0800

    chore: cleaning up code

--- Main.java
+++ Main.java
@@ -1,43 +1,45 @@
 import java.util.*;
 import library_package.*;
+
 public class Main{
 public static void main(String[] args){
     
     MainFrame myFrame = new MainFrame();
     myFrame.initialize();
 
-        Scanner sc = new Scanner(System.in);
+    try (Scanner sc = new Scanner(System.in)) {
         List<Book> library = new ArrayList<>();       
         
         for(int i = 0; i <= 5; i++){ //for loop for testing
-        System.out.println("Enter book name:");
-        String bookName = sc.nextLine();
-        
-        System.out.println("Enter author:");
-        String bookAuthor = sc.nextLine();
-        
-        System.out.println("Enter number of pages:");
-        int noOfPages = sc.nextInt();
-    
-        sc.nextLine();
-        
-        Book book = new Book();
-        book.setBookName(bookName);
-        book.setAuthor(bookAuthor);
-        book.setNoOfPages(noOfPages);
-        
+            System.out.println("Enter book name:");
+            String bookName = sc.nextLine();
+            
+            System.out.println("Enter author:");
+            String bookAuthor = sc.nextLine();
+            
+            System.out.println("Enter number of pages:");
+            int noOfPages = sc.nextInt();
+            
 
-        System.out.println("\nBook Details:");
-        System.out.println(book.getBookName() + " by: " + book.getAuthor() + "\nPage Counter: " + book.getNoOfPages());
-        
-        library.add(book);
-        
+            sc.nextLine();
+            
+            Book book = new Book();
+            book.setBookName(bookName);
+            book.setAuthor(bookAuthor);
+            book.setNoOfPages(noOfPages);
+            
+            
+            System.out.println("\nBook Details:");
+            System.out.println(book.getBookName() + " by: " + book.getAuthor() + "\nPage Counter: " + book.getNoOfPages());
+            
+            library.add(book);
+            
         }
         System.out.println("\nLibrary contains the following books:");
         for (Book b : library) {
             System.out.println(b.getBookName() + " by " + b.getAuthor());
 
         }
-        sc.close();
+    }
     }
 }
